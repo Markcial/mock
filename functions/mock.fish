5167432ccd68b8de5945aee85eae26b1b05a71fc
Marc Garcia <markcial@gmail.com>
2016-05-18 18:39:56 +0200

    Added possibility to use consecutive mocks for the same function, forbidding use of reserved commands

--- functions/mock.fish
+++ functions/mock.fish
@@ -1,28 +1,62 @@
-function mock
-  set -l _mock $argv[1]
+set -g _mocks
+
+function mock -a _mock -a result -a code -d "Mock library for fish shell testing"
+  set -l blacklisted "builtin" "functions" "eval" "command"
   set -l prefix __"$_mock"
+
+  # add result variable
   set -l result_var "$prefix"_result
+  if test -z "$result"
+    set result 0
+  end
+  if not set -q "$result_var"
+    set -g $result_var $result
+  else
+    set $result_var $$result_var $result
+  end
+
+  # add exec body variable
   set -l body_var "$prefix"_body
-  set -g "$result_var" 0
-  if test (count $argv) -ge 2
-    set "$result_var" $argv[2]
+  if test -z "$code"
+    set code " "
   end
-  set -g "$body_var"
-  if test (count $argv) -ge 3
-    set "$body_var" $argv[3]
+  if not set -q "$body_var"
+    set -g $body_var $code
+  else
+    set $body_var $$body_var $code
   end
 
-  function -V _mock "$_mock"
-    set -l name $_mock
-    set -l prefix __"$name"
-    set -l result_var "$prefix"_result
-    set -l body_var "$prefix"_body
+  if contains $_mock $blacklisted
+    echo The function '"'$_mock'"' is reserved and therefore cannot be mocked.
+    return 1
+  else if not contains $_mock $_mocks
+    function -V _mock $_mock
+      set -l name $_mock
+      set -l prefix __"$name"
+      set -l result_var "$prefix"_result
+      set -l body_var "$prefix"_body
+      set -l args $argv
+      set -l results (printf "%s\n" $$result_var)
+      set -l bodies (printf "%s\n" $$body_var)
 
-    set -l args $argv
-    set -l result $$result_var
-    set -l body $$body_var
-    eval $body
-    functions -e $name
-    return $result
+      set -l result $results[1]
+      set -l body $bodies[1]
+
+      set -e results[1]
+      set -e bodies[1]
+      set $result_var $results
+      set $body_var $bodies
+
+      eval $body
+      # check if the mock is contained in the _mocks var
+      set -l pos (contains --index -- $_mock $_mocks)
+      set -e _mocks[$pos]
+      if not contains -- $_mock $_mocks
+        functions -e $_mock
+      end
+
+      return $result
+    end
   end
+  set _mocks $_mocks $_mock
 end
--- test/mock.fish
+++ test/mock.fish
@@ -1,31 +1,69 @@
-test "$TEST It should be able to mock with exit success"
+function setup
+  set root (dirname (status -f))
+  source $root/functions/mock.fish
+end
+
+test "$TESTNAME It should be able to mock with exit success"
   (mock ls; and ls) $status -eq 0
 end
 
-test "$TEST It should be able to mock with fail status"
+test "$TESTNAME It should be able to mock with fail status"
    (mock ls 1; and ls) $status -eq 1
 end
 
-test "$TEST It should be able to mock with function body"
+test "$TESTNAME It should be able to mock with function body"
    hello = (mock ls 0 "echo hello"; and ls )
 end
 
-test "$TEST It should be able to use argument"
+test "$TESTNAME It should be able to use argument"
    "hello joe" = (mock ls 0 "echo hello \$args"; and ls joe )
 end
 
-test "$TEST It should be able to use some arguments"
+test "$TESTNAME It should be able to use some arguments"
    "hello joe" = (mock ls 0 "echo hello \$args[1]"; and ls joe mike )
 end
 
-test "$TEST It should be able to use multiple arguments"
+test "$TESTNAME It should be able to use multiple arguments"
    "hello joe and mike" = (mock ls 0 "echo hello \$args[1] and \$args[2]"; and ls joe mike )
 end
 
-test "$TEST It should be able to use nested functions"
+test "$TESTNAME It should be able to use nested functions"
    "1 2 3 4 5" = (mock ls 0 "echo (seq 5)"; and ls )
 end
 
-test "$TEST It should be able to use nested functions with arguments"
+test "$TESTNAME It should be able to use nested functions with arguments"
    "1 2 3 4 5" = (mock ls 0 "echo (seq \$args)"; and ls 5)
+end
+
+test "$TESTNAME It should be able to use nested functions with arguments"
+   "1 2 3 4 5" = (mock ls 0 "echo (seq \$args)"; and ls 5)
+end
+
+test "$TESTNAME It should not mock blacklisted elements 1"
+   (mock builtin) = "The function \"builtin\" is reserved and therefore cannot be mocked." -a $status -eq 1
+end
+
+test "$TESTNAME It should not mock blacklisted elements 2"
+   (mock functions) = "The function \"functions\" is reserved and therefore cannot be mocked." -a $status -eq 1
+end
+
+test "$TESTNAME It should not mock blacklisted elements 3"
+   (mock eval) = "The function \"eval\" is reserved and therefore cannot be mocked." -a $status -eq 1
+end
+
+test "$TESTNAME It should mock -n times"
+  "ok" "ok" = (
+    mock ls 0 "echo one"
+    mock ls 1 "echo two"
+    if test (ls) = "one" -a $status -eq 0
+      echo ok
+    else
+      echo ko
+    end
+    if test (ls) = "two" -a $status -eq 1
+      echo ok
+    else
+      echo ko
+    end
+  )
 end
